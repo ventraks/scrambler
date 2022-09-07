51afe6b78610cf02f12cd301f18f7ee30a9756cb
ventraks <83806248+ventraks@users.noreply.github.com>
2022-09-07 13:30:51 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+
+client_script 'client.lua'
+
+ui_page {
+  'html/index.html'
+}
+
+files {
+    'html/script.js',
+    'html/index.html',
+    'html/style.css',
+}
